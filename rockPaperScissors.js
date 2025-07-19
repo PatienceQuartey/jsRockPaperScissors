1112da8436a2be29cc3f2f7654976df22cafcfff
Patience Quartey <patiencenkquartz@gmail.com>
2025-07-19 12:33:40 +0100

    Added spacing and semi-colons for good practice working.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,41 +1,47 @@
 function getComputerChoice(computerChoice){
     let weRandoNum = Math.random() * 3;
-    let weFloorNum = Math.floor(weRandoNum) + 1
-    console.log(weFloorNum)
+    let weFloorNum = Math.floor(weRandoNum) + 1;
+    console.log(weFloorNum); // to confirm number number being outputted can be commented out on project completion
 
     if(weFloorNum === 1){
-       computerChoice = "Rock"
-       return computerChoice 
+       computerChoice = "Rock";
+       return computerChoice;
+
     } else if(weFloorNum === 2){
-        computerChoice = "Paper"
-        return computerChoice
+        computerChoice = "Paper";
+        return computerChoice;
+
     } else if(weFloorNum === 3){
-        computerChoice = "Scissors"
-        return computerChoice
+        computerChoice = "Scissors";
+        return computerChoice;
+
     }
 
 }
 
-console.log(getComputerChoice())
+console.log(getComputerChoice());
 
 
 
 function getHumanChoice(userChoice){
-    const userPrompt = Number(prompt("Enter a Number ranging from 1 to 3.")) 
+    const userPrompt = Number(prompt("Enter a Number ranging from 1 to 3.")); 
     //console.log(typeof userPrompt) //to check if this is the correct type.
-    console.log(userPrompt) 
+    console.log(userPrompt) // to confirm number being outputted to console. Can be commented out on project completion
 
     if(userPrompt === 1){
-        userChoice = "Rock"
-        return userChoice
+        userChoice = "Rock";
+        return userChoice;
+
     } else if(userPrompt === 2){
-        userChoice = "Paper"
-        return userChoice
+        userChoice = "Paper";
+        return userChoice;
+
     } else if(userPrompt === 3){
-        userChoice = "Scissors"
-        return userChoice
+        userChoice = "Scissors";
+        return userChoice;
+        d
     }
 
 
 }
-console.log(getHumanChoice())
+console.log(getHumanChoice());
