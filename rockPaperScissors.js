320e67db8c50b4763fac8f598202bb631d6d41a5
Patience Quartey <patiencenkquartz@gmail.com>
2025-07-19 13:27:42 +0100

    Edited file to remove user prompt form getHumanChoice function.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,7 @@
 function getComputerChoice(computerChoice){
     let weRandoNum = Math.random() * 3;
     let weFloorNum = Math.floor(weRandoNum) + 1;
-    console.log(weFloorNum); // to confirm number number being outputted can be commented out on project completion
+    // console.log(weFloorNum); // to confirm number number being outputted can be commented out on project completion
 
     if(weFloorNum === 1){
        computerChoice = "Rock";
@@ -22,12 +22,13 @@ function getComputerChoice(computerChoice){
 console.log(getComputerChoice());
 
 
+const userPrompt = Number(prompt("Enter a Number ranging from 1 to 3.")); 
+    //console.log(typeof userPrompt) //to check if this is the correct type.
+    // console.log(userPrompt) // to confirm number being outputted to console. Can be commented out on project completion
+
 
 function getHumanChoice(userChoice){
-    const userPrompt = Number(prompt("Enter a Number ranging from 1 to 3.")); 
-    //console.log(typeof userPrompt) //to check if this is the correct type.
-    console.log(userPrompt) // to confirm number being outputted to console. Can be commented out on project completion
-
+    
     if(userPrompt === 1){
         userChoice = "Rock";
         return userChoice;
@@ -48,6 +49,3 @@ console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
-
-
-function playRound(humanChoice , computerChoice){};
