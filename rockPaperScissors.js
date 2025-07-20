23113754438f7dc2198c5614e624f3f9cd95955f
Patience Quartey <patiencenkquartz@gmail.com>
2025-07-20 08:58:44 +0100

    Added function for user's Choice, using charAt(), toLowerCase() and toUpperCase() methods to ensure uppercase first letter and lowercase for remaining letters regaardless of whether inputs in all uppercase, lowecase or both.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -20,3 +20,17 @@ function getComputerChoice(computerChoice){
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice(userChoice){
+    userPrompt= prompt("Welcome to Rock, Paper, Scissors. To start the game, type in 'Rock', 'Paper' or 'Scissors in the box below: ");
+    userChoice = userPrompt;
+    userWord = userChoice.charAt(0);
+    userWord = userWord.toUpperCase();
+    remUserWords = userChoice.slice(1);
+    remUserWords = remUserWords.toLowerCase();
+    console.log(userWord + remUserWords);
+
+
+}
+getHumanChoice();
+
