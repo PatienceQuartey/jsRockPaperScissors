0c5f926dd7b6f8ecd6efab31fa76feb5b08744c3
Patience Quartey <patiencenkquartz@gmail.com>
2025-07-24 21:43:37 +0100

    feat: logic overhaul to console game
    - Created forComputer() function that housed random number generation
    for computer.
    -Used if statements in forComputer() to convert generated numbers to
    strings.
    - Created forUser() function that prompted user to input information.
    - In forUser(), used charAt, slice and lowercase/uppercase to ensure
     all inputs show with 1st letter uppercase and all other letters in
      lowecase.
    - created playRound() function that took two parameters.
    - The playRound() fucntiopn compared string input from user to computer
     choice to determine who won the round or if there was a tie.
     - Created two variables to hold scores for users and computer.
     originally had const variables (computerSelection and humanSelection)
      to assign value of forComputer() and forUser() respectively.
    - Created playGame() function.
    - Moved User and computer score tally into playGame().
    - changed const variables to let to allow change in function values.
    - Added for loop for game to loop through game 5 times.
    - Moved playRound() into the for loop for rounds to be repeated.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,36 +1,116 @@
-function getComputerChoice(computerChoice){
-    theFlooredNumber = Math.floor(Math.random() * 3) + 1;
-    computerChoice = theFlooredNumber;
 
-    if( computerChoice == 1){
-        computerChoice = "Rock";
-        return computerChoice;
 
-    }else if(computerChoice == 2){
-        computerChoice = "Paper";
-        return computerChoice;
+function forComputer(compChoice){
+    floorNumber = Math.floor(Math.random() * 3) + 1;
+    compChoice = floorNumber;
+    //console.log(compChoice)
+    if(compChoice == 1){
+        compChoice = "Rock";
+        console.log(compChoice)
+        return compChoice;
 
-    }else if(computerChoice == 3){
-        computerChoice = "Scissors";
-        return computerChoice;
+    }else if(compChoice == 2){
+        compChoice = "Paper";
+        console.log(compChoice)
+        return compChoice;
 
+    }else if( compChoice == 3){
+        compChoice = "Scissors";
+        console.log(compChoice)
+        return compChoice;
     }
 
-    return computerChoice;
+    
+
 }
 
-console.log(getComputerChoice())
-
-function getHumanChoice(userChoice){
-    userPrompt= prompt("Welcome to Rock, Paper, Scissors. To start the game, type in 'Rock', 'Paper' or 'Scissors in the box below: ");
-    userChoice = userPrompt;
-    userWord = userChoice.charAt(0);
-    userWord = userWord.toUpperCase();
-    remUserWords = userChoice.slice(1);
-    remUserWords = remUserWords.toLowerCase();
-    console.log(userWord + remUserWords);
+/* ############### FOR SPACE ############ */
+/* ############### FOR SPACE ############ */
+function forUser(userChoice){
+    thePrompt = prompt("Rock, Paper or Scissor");
+    promptWord = thePrompt.charAt(0);
+    promptWord = promptWord.toUpperCase();
+    restOfWords = thePrompt.slice(1);
+    restOfWords = restOfWords.toLowerCase();
+    userChoice = promptWord + restOfWords;
+    console.log(userChoice)
+    return userChoice;
 }
-getHumanChoice();
+/* ############### FOR SPACE ############ */
+/* ############### FOR SPACE ############ */
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    /* ############### FOR SPACE ############ */
+    /* ############### FOR SPACE ############ */
+    for(let theRound = 0; theRound < 5; theRound +1){
+        let computerSelection = forComputer();
+        let  humanSelection = forUser();
+         /* ############### FOR SPACE ############ */
+        /* ############### FOR SPACE ############ */
+        function playRound(computerChoice , humanChoice){
+            if(computerChoice == humanChoice){
+                console.log(`The Computer chose ${computerChoice} and You chose ${humanChoice} - This is a tie.`);
+                console.log(`Computer Score ${computerScore}`);
+                console.log(`Your Score ${humanScore}`);
 
-let humanScore = 0;
-let computerScore = 0;
+            }else if(computerChoice == "Rock" && humanChoice == "Scissors"){
+                console.log(`The Computer chose ${computerChoice} and You chose ${humanChoice} - Rock bests Scissors so Computer wins this round.`);
+                computerScore +=1;
+                console.log(`Computer Score ${computerScore}`);
+                console.log(`Your Score ${humanScore}`);
+
+            }else if(computerChoice == "Paper" && humanChoice == "Rock"){
+                console.log(`The Computer chose ${computerChoice} and You chose ${humanChoice} - Paper beats Rock so Computer wins this round.`);
+                computerScore +=1;
+                console.log(`Computer Score ${computerScore}`);
+                console.log(`Your Score ${humanScore}`);
+
+            }else if(computerChoice == "Scissors" && humanChoice == "Paper"){
+                console.log(`The Computer chose ${computerChoice} and You chose ${humanChoice} - Scissors beats Paper so Computer wins this round.`);
+                computerScore +=1;
+                console.log(`Computer Score ${computerScore}`);
+                console.log(`Your Score ${humanScore}`);
+
+            }else if(humanChoice == "Rock" && computerChoice == "Scissor"){
+                console.log(`The Computer chose ${computerChoice} and You chose ${humanChoice} - Rock bests Scissors so You win this round.`);
+                humanScore +=1;
+                console.log(`Computer Score ${computerScore}`);
+                console.log(`Your Score ${humanScore}`);
+
+            }else if(humanChoice == "Paper" && computerChoice == "Rock"){
+                console.log(`The Computer chose ${computerChoice} and You chose ${humanChoice} - Paper beats Rock so You win this round.`);
+                humanScore +=1;
+                console.log(`Computer Score ${computerScore}`);
+                console.log(`Your Score ${humanScore}`);
+
+            }else if(humanChoice == "Scissors" && computerChoice == "Paper"){
+                console.log(`The Computer chose ${computerChoice} and You chose ${humanChoice} - Scissors beats Paper so You win this round.`);
+                humanScore +=1;
+                console.log(`Computer Score ${computerScore}`);
+                console.log(`Your Score ${humanScore}`);
+
+            };
+            /* ############### FOR SPACE ############ */
+            if(humanChoice){
+                let userWord = humanChoice.charAt(0);
+                userWord = userWord.toUpperCase();
+                let remWords = humanChoice.slice(1);
+                remWords = remWords.toLowerCase();
+                return userWord + remWords;
+
+            };
+
+            gameRounds -= 1;
+            
+
+        }
+        playRound(computerSelection , humanSelection);
+        /* ############### FOR SPACE ############ */
+        console.log(theRound +=1);
+
+    }
+    
+}
+
+playGame()
